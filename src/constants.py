362bd2a3d60d24edc19960627eb35cf46597ee52
Eva Lott <evalott100@gmail.com>
2023-01-11 15:48:12 +0000

    #1 ran more tests

--- src/constants.py
+++ src/constants.py
@@ -23,7 +23,7 @@ FACTORS_OF_CHUNKS_TO_DELETE = [3, 5]
 ZMQ_PORT = 5556
 
 # Delete previous runs when the proprtion of space left is
-DELETE_WHEN_PROPORTION_SPACE_LEFT = 0.8
+DELETE_WHEN_PROPORTION_SPACE_LEFT = 0.1
 
 # Number of runs to delete when capacity has been reached
 NUMBER_OF_RUNS_TO_DELETE = 2
